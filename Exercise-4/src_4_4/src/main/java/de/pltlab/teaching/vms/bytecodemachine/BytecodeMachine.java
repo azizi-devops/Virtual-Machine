1a8c1aa6107a6833b0fafbcbf745f8cee5e5f6dc
karim <karim.azizi.master@gmail.com>
2026-06-13 11:21:23 +0200

    exercise 4.4

---
+++ Exercise-4/src_4_4/src/main/java/de/pltlab/teaching/vms/bytecodemachine/BytecodeMachine.java
@@ -0,0 +1,27 @@
+package de.pltlab.teaching.vms.bytecodemachine;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.objectweb.asm.Type;
+
+public class BytecodeMachine {
+	
+	public static void main(String[] args) throws IOException {
+//		File file = Path.of("target/test-classes/Simple.class".replace('/', File.separatorChar)).toFile();
+//		File file = Path.of("target/test-classes/GGT.class".replace('/', File.separatorChar)).toFile();
+//		File file = Path.of("target/test-classes/GGT2.class".replace('/', File.separatorChar)).toFile();
+//		File file = Path.of("target/test-classes/Factorial.class".replace('/', File.separatorChar)).toFile();
+		File file = Path.of("target/test-classes/ManyBlocks.class".replace('/', File.separatorChar)).toFile();
+//		File file = Path.of("target/test-classes/Prime.class".replace('/', File.separatorChar)).toFile();
+
+		Interpreter interpreter = new DecodeDispatchInterpreterProvided (file);
+//		Interpreter interpreter = new IndirectThreadedIntepreter(file);
+//		Interpreter interpreter = new DirectThreadedIntepreter(file);
+
+		interpreter.setArguments(new String[] {"3", "30" });
+		interpreter.run();
+	}
+	
+}
